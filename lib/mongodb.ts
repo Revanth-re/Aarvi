d1b44d13c67eca5aea5e75be20f81e86664c3257
Revanth-re <revanthrevi131@gmail.com>
2026-06-01 19:06:44 +0530

    mongo connection fixed

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,12 +1,62 @@
+// import mongoose from "mongoose";
+// const URI ="mongodb+srv://aarvi:aarvi%401206@aarvi-cluster.n3h9ppt.mongodb.net/aarvi?retryWrites=true&w=majority&appName=aarvi-cluster";
+// interface Cache { conn: typeof mongoose | null; promise: Promise<typeof mongoose> | null; }
+// declare global { var __mongoose: Cache; }
+// const cache: Cache = global.__mongoose || { conn: null, promise: null };
+// if (!global.__mongoose) global.__mongoose = cache;
+// export async function connectDB() {
+//   if (cache.conn) return cache.conn;
+//   if (!cache.promise) cache.promise = mongoose.connect(URI, { bufferCommands: false });
+//   cache.conn = await cache.promise;
+//   return cache.conn;
+// }
 import mongoose from "mongoose";
-const URI = process.env.MONGODB_URI || "mongodb://localhost:27017/naad";
-interface Cache { conn: typeof mongoose | null; promise: Promise<typeof mongoose> | null; }
-declare global { var __mongoose: Cache; }
+
+// Properly encode the password
+// "aarvi@1206" should be encoded as "aarvi%401206" (you have this correct)
+// But make sure the entire string is properly formatted
+const URI = "mongodb+srv://aarvi:aarvi%401206@aarvi-cluster.n3h9ppt.mongodb.net/aarvi?retryWrites=true&w=majority&appName=aarvi-cluster";
+
+interface Cache { 
+  conn: typeof mongoose | null; 
+  promise: Promise<typeof mongoose> | null; 
+}
+
+declare global { 
+  var __mongoose: Cache; 
+}
+
 const cache: Cache = global.__mongoose || { conn: null, promise: null };
-if (!global.__mongoose) global.__mongoose = cache;
+
+if (!global.__mongoose) {
+  global.__mongoose = cache;
+}
+
 export async function connectDB() {
-  if (cache.conn) return cache.conn;
-  if (!cache.promise) cache.promise = mongoose.connect(URI, { bufferCommands: false });
-  cache.conn = await cache.promise;
-  return cache.conn;
+  try {
+    if (cache.conn) {
+      console.log("Using existing database connection");
+      return cache.conn;
+    }
+    
+    if (!cache.promise) {
+      console.log("Creating new database connection...");
+      cache.promise = mongoose.connect(URI, { 
+        bufferCommands: false,
+        // Add these options for better reliability
+        serverSelectionTimeoutMS: 5000,
+        socketTimeoutMS: 45000,
+      });
+    }
+    
+    cache.conn = await cache.promise;
+    console.log("Database connected successfully");
+    return cache.conn;
+  } catch (error) {
+    console.error("Database connection error:", error);
+    // Reset cache on error
+    cache.promise = null;
+    cache.conn = null;
+    throw error;
+  }
 }
