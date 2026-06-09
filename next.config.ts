5ec157b912e114ea7dbb35e503a9a0fa451440e8
Revanth-re <revanthrevi131@gmail.com>
2026-06-09 19:20:52 +0530

    fixed limit size

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol:"https", hostname:"images.unsplash.com" },
-      { protocol:"https", hostname:"**.cloudinary.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "**.cloudinary.com" },
     ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "50mb",
+    },
   },
 };
 export default nextConfig;
